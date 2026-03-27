2ef81215775d2b2a4068ed40a192e18a3974daed
Michael Thiesen <michael@thiesen.dev>
2026-03-27 15:59:21 -0700

    chore: clean up tsconfig and remove dead files
    Remove options redundant with strict (alwaysStrict, noImplicitAny,
    noImplicitThis) and allowJs (no JS source files remain). Delete
    orphaned test/.eslintrc.js and unused test/setup/ scripts.

--- test/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    jest: true,
-  },
-  rules: {
-    'func-names': ['off'],
-  },
-};
--- test/setup/run-simulator.js
+++
@@ -1,3 +0,0 @@
-const { startSimulator } = require('./simulator');
-
-startSimulator();
